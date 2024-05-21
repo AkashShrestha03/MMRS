d17b5edb9fc9eb61ea19d83e29aa532e62692ea2
Akash Shrestha <akashshrestha0399@gmail.com>
2024-05-21 23:10:53 +0530

    chng

--- src/assets/js/slick.js
+++ src/assets/js/slick.js
@@ -3,7 +3,7 @@
   "function" == typeof define && define.amd
     ? define(["jquery"], i)
     : "undefined" != typeof exports
-    ? (module.exports = i(require("jquery")))
+    ? (module.exports = i(("jquery")))
     : i(jQuery);
 })(function (i) {
   "use strict";
